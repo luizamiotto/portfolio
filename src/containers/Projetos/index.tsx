df3bbb0c97252ed06eb1140ee4aa38b9f2705706
Luiza Miotto <luu.miotto@gmail.com>
2023-12-19 00:00:59 -0300

    estilizando a sidebar

--- src/containers/Projetos/index.tsx
+++ src/containers/Projetos/index.tsx
@@ -30,6 +30,15 @@ const Projetos = () => (
       <li>
         <Projeto />
       </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
     </Lista>
   </section>
 )
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/lumiotto.png" />
-    <Titulo fontSize={20}>Luiza Miotto</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Luiza Miotto</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        lumiotto
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedora web
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
